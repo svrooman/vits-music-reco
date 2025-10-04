22b8e3911989311fe0e8daddf826b47a8d3a07fa
Scott Vrooman <svrooman@vits.space>
2025-10-04 12:43:09 -0700

    Remove defer from Alpine scripts to fix modal initialization

--- resources/views/components/layouts/music.blade.php
+++ resources/views/components/layouts/music.blade.php
@@ -70,8 +70,8 @@
     </footer>
 
     @livewireScripts
-    <script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/focus@3.x.x/dist/cdn.min.js"></script>
-    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@alpinejs/focus@3.x.x/dist/cdn.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     @livewire('wire-elements-modal')
 </body>
 </html>
