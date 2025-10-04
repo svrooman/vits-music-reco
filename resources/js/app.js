0fd92f68e69c86aeca407d9f43992345237a861d
Scott Vrooman <svrooman@vits.space>
2025-10-04 12:51:10 -0700

    Bundle wire-elements modal JS to prevent duplicate Alpine instances

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import Sortable from 'sortablejs';
 import Alpine from 'alpinejs';
 import focus from '@alpinejs/focus';
+import '../../vendor/wire-elements/modal/resources/js/modal';
 
 // Register Alpine plugins
 Alpine.plugin(focus);
